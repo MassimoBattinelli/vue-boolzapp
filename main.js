edb734534ccd6d4d69cfc105ceac96a1a2b5f0f5
massi <massimo.battinelli@gmail.com>
2022-03-16 13:12:43 +0100

    fix dei file. da risolvere problema icone su js

---
+++ main.js
@@ -0,0 +1,126 @@
+var app= new Vue({
+    el: '#app',
+    data: {
+      indexChat:0,
+      Newtext:"",
+      searchName:"",
+  
+      user:{
+          name: 'Massimo',
+          surname: '',
+        },
+
+      contacts: [
+        {
+          name: "Michele",
+          avatar: 'img/avatar2.jpg',
+          visible: true,
+          find:true,
+  
+  
+          log: "ultimo accesso oggi alle: "+ dayjs(new Date()).subtract(20,'minute').format('HH:mm:ss').toString(),
+          messages: [
+            {
+              date: '16/03/2022 15:30:55',
+              text: 'Lorem ipsum dolor sit amet consectetur ad',
+              status: 'sent',
+              isHiddenOptions: true,
+            },
+            {
+              date: '16/03/2022 15:30:55',
+              text: 'Lorem ipsum dolor sit amet consectetur ad',
+              status: 'sent',
+              isHiddenOptions: true,
+            },
+            {
+              date: '16/03/2022 15:30:55',
+              text: 'Lorem ipsum dolor sit amet consectetur ad',
+              status: 'received',
+              isHiddenOptions: true,
+            }
+          ],
+        },
+        {
+          name: 'Fabio',
+          avatar: 'img/avatar3.jpg',
+          visible: false,
+          find:true,
+          log: "ultimo accesso oggi alle: "+ dayjs(new Date()).subtract(126,'minute').format('HH:mm:ss').toString(),
+          messages: [
+            {
+              date: '16/03/2022 15:30:55',
+              text: 'Lorem ipsum dolor sit amet consectetur ad',
+              status: 'sent',
+              isHiddenOptions: true,
+            },
+            {
+              date: '16/03/2022 15:30:55',
+              text: 'Lorem ipsum dolor sit amet consectetur ad',
+              status: 'received',
+              isHiddenOptions: true,
+            },
+            {
+              date: '16/03/2022 15:30:55',
+              text: 'Lorem ipsum dolor sit amet consectetur ad',
+              status: 'sent',
+              isHiddenOptions: true,
+            }
+          ],
+        },
+      ],
+    },
+  
+    methods: {
+
+      activeChat: function (index) {
+  
+  
+        this.contacts.forEach((element) => {
+          element.visible=false;
+        });
+  
+        this.contacts[index].visible=true;
+        this.indexChat=index;
+  
+      },
+
+      submit: function (text,date,status) {
+  
+          text=this.Newtext,
+          date=dayjs(new Date()).format('DD/MM/YYYY HH:mm:ss');
+          status='received';
+          isHiddenOptions= true,
+          this.contacts[this.indexChat].messages.push(
+            {
+              text,
+              date,
+              status,
+              isHiddenOptions,
+            }
+          );
+          this.newSentMesseage();
+  
+  
+      },
+
+      newSentMesseage: function (text,date,status) {
+          var adresses=this.contacts[this.indexChat].messages;
+          setTimeout(function(){
+          text="OK";
+          date=dayjs(new Date()).format('DD/MM/YYYY HH:mm:ss');
+          status='sent';
+          isHiddenOptions= true,
+          adresses.push(
+            {
+              text,
+              date,
+              status,
+              isHiddenOptions,
+            }
+          )
+          }, 1000);
+  
+      },
+
+    },
+  })
